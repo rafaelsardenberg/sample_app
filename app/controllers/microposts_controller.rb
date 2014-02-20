210efa48b5cedf06a408b9ea05241f64f71bf6c9
rafaelsardenberg <sardenbergrafael@gmail.com>
2014-02-20 10:51:58 +0100

    Add user following

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -24,4 +24,4 @@ class MicropostsController < ApplicationController
 			@micropost = current_user.microposts.find_by_id(params[:id])
 			redirect_to root_path if @micropost.nil?
 		end
-end
+end	
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,28 +31,35 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
 
   it { should be_valid }
   it { should_not be_admin }
 
   describe "with admin attribute set to 'true'" do
-    before { @user.toggle!(:admin)}
+    before { @user.toggle!(:admin) }
 
-    it {should be_admin}
+    it { should be_admin }
   end
 
   describe "when name is not present" do
-  	before { @user.name = ""}
+  	before { @user.name = "" }
   	it { should_not be_valid }
   end
 
   describe "when email is not present" do
-  	before { @user.email = ""}
+  	before { @user.email = "" }
   	it { should_not be_valid }
   end
 
   describe "when name is too long" do
-  	before { @user.name = "a"*51}
+  	before { @user.name = "a"*51 }
   	it { should_not be_valid }
   end
 
@@ -92,17 +99,17 @@ describe User do
   end
 
   describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch"}
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 
   describe "when password confirmation is nil" do
-    before { @user.password_confirmation = nil}
+    before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
 
   describe "with a password too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5}
+    before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
 
@@ -118,7 +125,7 @@ describe User do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
       it { should_not == user_for_invalid_password }
-      specify { user_for_invalid_password.should be_false}
+      specify { user_for_invalid_password.should be_false }
     end
   end
     
@@ -128,7 +135,7 @@ describe User do
   end
 
   describe "micropost associations" do
-    before { @user.save}
+    before { @user.save }
     let!(:older_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     end
@@ -152,10 +159,44 @@ describe User do
       let(:unfollowed_post) do 
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem Ipsum") }
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
-      its(:feed) { should_not include(unfollowed_post)}
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) } 
     end
   end
 end
